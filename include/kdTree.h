a71b441db86d6e12064f787f4644bd61f38f4e95
sclosaf <sclosaf@gmail.com>
2026-02-20 12:19:59 +0100

    Added firsts structs and types renames

--- include/kdTree.h
+++ include/kdTree.h
@@ -1,3 +1,48 @@
 #ifndef KDTREE_H
 #define KDTREE_H
+
+// IDs?
+
+typedef struct
+{
+    f32 coords[];
+} point;
+
+typedef struct
+{
+    struct KDNode* root;
+
+    size_t totalPoints;
+    size_t totalNodes;
+
+    u32 dimensions;
+} KDTree;
+
+typedef struct
+{
+    i8 splitDim;
+    i8 splitValue;
+
+    struct KDNode* left;
+    struct KDNode* right;
+    struct KDNode* parent;
+
+    struct approxCounter size;
+} KDNode;
+
+typedef struct
+{
+    point** points;
+    u32 pointCount;
+
+    struct KDNode* parent;
+} KDLeaf;
+
+typedef struct {
+    u32 value;
+    u32 totalTreeSize;
+    f32 beta;
+    u32 lastUpdate;
+} approxCounter;
+
 #endif
