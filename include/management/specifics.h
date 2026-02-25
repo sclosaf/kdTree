0645f39ce6549d58246b5ec8a4b5ff07b0aade81
sclosaf <sclosaf@gmail.com>
2026-02-25 21:57:40 +0100

    Correction

--- include/management/specifics.h
+++ include/management/specifics.h
@@ -5,8 +5,8 @@
 
 #include "utils/types.h"
 
-void printInfoSystem()
-void printRankInfo(struct dpu_set_t rank, u32 rank_id)
-void printDpuInfo(struct dpu_set_t dpu, u32 dpu_id, u32 rank_id)
+void printInfoSystem();
+void printRankInfo(struct dpu_set_t rank, u32 rank_id);
+void printDpuInfo(struct dpu_set_t dpu, u32 dpu_id, u32 rank_id);
 
 #endif
