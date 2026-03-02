a81dbaea22de8fa44b4486395a3ad337ceac5bb6
sclosaf <sclosaf@gmail.com>
2026-03-02 10:42:05 +0100

    Correction

--- include/kdTree/utils.h
+++ include/kdTree/utils.h
@@ -1,6 +1,11 @@
 #ifndef KDTREE_UTILS_H
 #define KDTREE_UTILS_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+#include "kdTree/types.h"
+
 float findMedian(point** points, size_t start, size_t end, uint8_t dim);
 uint8_t findSplitDim(point** points, size_t start, size_t end);
 int16_t findGroup(size_t size, KDGroup** groups, uint8_t numGroups);
