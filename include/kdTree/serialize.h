ffa1aa8ed1aac606e584ad042ae56d28de18ce3a
sclosaf <sclosaf@gmail.com>
2026-03-02 11:06:34 +0100

    Correction

--- include/kdTree/serialize.h
+++ include/kdTree/serialize.h
@@ -1,6 +1,11 @@
 #ifndef KDTREE_SERIALIZE_H
 #define KDTREE_SERIALIZE_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+#include "kdTree/types.h"
+
 void serializeNodeData(KDNode* node, uint8_t** ptr);
 void serializeNodeSize(KDNode* node, size_t* size);
 void* serializeTree(KDNode* root, size_t* size);
