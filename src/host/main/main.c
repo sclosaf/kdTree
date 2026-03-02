d8e0d3c4c49ceb8efc52e02a9110bf9b894af376
sclosaf <sclosaf@gmail.com>
2026-03-02 11:24:01 +0100

    Correction

--- src/host/main/main.c
+++ src/host/main/main.c
@@ -4,21 +4,32 @@
 
 int main()
 {
-    struct dpu_set_t all_dpus;
-    uint32_t nr_dpus;
+    struct dpu_set_t dpu_set, rank, dpu;
+    uint32_t nr_dpus, nr_ranks;
+    uint32_t rank_id = 0, dpu_id = 0;
 
-    DPU_ASSERT(dpu_probe(NULL, &all_dpus, &nr_dpus));
+    DPU_ASSERT(dpu_alloc(DPU_ALLOCATE_ALL, NULL, &dpu_set));
 
-    printf("Total number of DPUs available: %u\n", nr_dpus);
+    DPU_ASSERT(dpu_get_nr_dpus(dpu_set, &nr_dpus));
+    DPU_ASSERT(dpu_get_nr_ranks(dpu_set, &nr_ranks));
 
-    struct dpu_set_t chip_dpus;
-    DPU_FOREACH(dpu, all_dpus) {
-        struct dpu_t *dpu_ptr;
-        dpu_ptr = &dpu;
-        printf("Chip ID: 0x%x\n", dpu_ptr->chip_id);
-        printf("Rank number: %u\n", dpu_ptr->rank->nr_of_dpus);
+    printf("Total DPUs: %u\n", nr_dpus);
+    printf("Total ranks: %u\n", nr_ranks);
+
+    DPU_RANK_FOREACH(dpu_set, rank)
+    {
+        uint32_t per_rank;
+        DPU_ASSERT(dpu_get_nr_dpus(rank, &per_rank));
+        printf("Rank %u: %u DPUs\n", rank_id++, per_rank);
     }
 
-    dpu_free(all_dpus);
+    DPU_FOREACH(dpu_set, dpu)
+    {
+        printf("DPU %u present\n", dpu_id);
+        dpu_id++;
+    }
+
+    DPU_ASSERT(dpu_free(dpu_set));
+
     return 0;
 }
