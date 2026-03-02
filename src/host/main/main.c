157aca639c56d884f091bfa95ab4a2f6a854a010
sclosaf <sclosaf@gmail.com>
2026-03-02 11:16:51 +0100

    Correction

--- src/host/main/main.c
+++ src/host/main/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <dpu.h>
 
-int main(int argc, char** argv)
+int main()
 {
-    printf("Hello world!");
+    struct dpu_set_t all_dpus;
+    uint32_t nr_dpus;
+
+    DPU_ASSERT(dpu_probe(NULL, &all_dpus, &nr_dpus));
+
+    printf("Total number of DPUs available: %u\n", nr_dpus);
+
+    struct dpu_set_t chip_dpus;
+    DPU_FOREACH(dpu, all_dpus) {
+        struct dpu_t *dpu_ptr;
+        dpu_ptr = &dpu;
+        printf("Chip ID: 0x%x\n", dpu_ptr->chip_id);
+        printf("Rank number: %u\n", dpu_ptr->rank->nr_of_dpus);
+    }
+
+    dpu_free(all_dpus);
     return 0;
 }
