edf9cdccea18f7d3769d62e791be4ebda51977ff
sclosaf <sclosaf@gmail.com>
2026-03-02 10:37:41 +0100

    Correction

--- include/kdTree/free.h
+++ include/kdTree/free.h
@@ -1,6 +1,8 @@
 #ifndef KDTREE_FREE_H
 #define KDTREE_FREE_H
 
+#include "kdTree/types.h"
+
 void freeKDTree(KDNode* node);
 void freeMatrix(void** matrix, size_t rows);
 void freeLeafNode(KDNode* node);
