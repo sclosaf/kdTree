701b35c7c380ef5731efd7ba97a7455261533835
sclosaf <sclosaf@gmail.com>
2026-03-03 14:14:55 +0100

    Correction

--- src/dpu/wramProbe.c
+++ src/dpu/wramProbe.c
@@ -6,17 +6,14 @@ static uint32_t measureWramAvailable(void)
 {
     uint32_t total = 0;
     uint32_t minBlock = 8;
-    uint32_t step = 4096;
+    uint32_t step = 1024;
     void *ptr;
 
     while (step >= minBlock)
     {
         ptr = mem_alloc(step);
         if(ptr != NULL)
-        {
             total += step;
-            mem_reset();
-        }
         else
             step /= 2;
     }
