44d1c772b692b96f56acce25bee2388fe3ed6db5
sclosaf <sclosaf@gmail.com>
2026-03-03 10:28:07 +0100

    Added test metrics

--- include/management/metrics.h
+++ include/management/metrics.h
@@ -1,6 +1,8 @@
 #ifndef MANAGEMENT_SPECIFICS_H
 #define MANAGEMENT_SPECIFICS_H
 
+#include <stdint.h>
+
 unsigned long long getTotalRam();
 unsigned long long getAvailableRam();
 uint32_t getNumDPUs();
