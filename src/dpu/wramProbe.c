3ff7cd02b6ca7b27ddeb7547637d55f7131e9c89
sclosaf <sclosaf@gmail.com>
2026-03-03 14:47:45 +0100

    Correction

--- src/dpu/wramProbe.c
+++ src/dpu/wramProbe.c
@@ -2,23 +2,28 @@
 #include <mram.h>
 #include <defs.h>
 
-static uint32_t measureWramAvailable(void)
+static uint32_t measureWramAvailable()
 {
     uint32_t total = 0;
     uint32_t minBlock = 1;
-    uint32_t step = 63;
-    void *ptr;
+    uint32_t step = 1024;
 
     while(step >= minBlock)
     {
-        ptr = mem_alloc(step);
-        if(ptr)
+        volatile uint8_t arr[step];
+
+        arr[0] = 0xAA;
+        arr[step - 1] = 0xBB;
+
+        if(arr[0] == 0xAA && arr[step - 1] == 0xBB)
+        {
             total += step;
+            step = step * 2;
+        }
         else
             step /= 2;
     }
 
-    mem_reset();
     return total;
 }
 
